574177699ecd34aa28c1afd31ce8e402023bc0a8
haozai <752057574@qq.com>
2021-07-02 12:31:03 +0800

    'day1'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab_bar_item" @click="ItemClick">
+    <div v-show="!isActive"><slot name="item-icon"></slot></div>
+    <div v-show="isActive"><slot name="item-icon-active"></slot></div>
+    <div :style="itemactive"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    link: String,
+    activecolor: {
+      type: String,
+      default: 'deeppink'
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    itemactive() {
+      return this.isActive? {color: this.activecolor} : ''
+    },
+    isActive() {
+      return this.$route.path.indexOf(this.link)!==-1
+    }
+  },
+  methods: {
+    ItemClick() {
+      // console.log(this.link)
+      this.$router.replace(this.link)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab_bar_item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab_bar_item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
+
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/shopcart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
